85bd38f720198366a2503ef08db47c282943bd53
hchampagne <hchampagne56@gmail.com>
2019-09-30 21:26:53 +0200

    pagination essai  ???

--- application/controllers/Pagination.php
+++ application/controllers/Pagination.php
@@ -1,28 +1,14 @@
 	
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-
-
 class Pagination extends CI_Controller
 {
 
-
-
     public function __construct()
     {
-
         parent::__construct();
 
-
-
-        //$this->load->helper('url');
-
-        //$this->load->model('pagination_model');
-
-        //$this->load->library("pagination");
     }
-
-
 
     public function index()
     {
@@ -30,10 +16,39 @@ class Pagination extends CI_Controller
         $config = array();
         $config["base_url"] = site_url() . "/pagination";
         $config["total_rows"] = $this->pagination_model->get_counter();
-        $config["per_page"] = 10;
+        $config["per_page"] = 7;
         $config["uri_segment"] = 2;
 
 
+        //$config['full_tag_open'] = '<ul class="pagination">';
+        //$config['full_tag_close'] = '</ul>';
+
+        $config['use_page_numbers'] = TRUE;
+
+        $config['first_link'] = false;
+        //$config['first_tag_open'] = '';
+        //$config['first_tag_close'] = '';
+
+        $config['last_link'] = false;
+        //$config['last_tag_open'] = '';
+        //$config['last_tag_close'] = '';
+
+
+        $config['next_link'] = 'Suivant';
+        //$config['next_tag_open'] = '';
+        //$config['next_tag_close'] = '';
+
+        $config['prev_link'] = 'Précédent';
+        //config['prev_tag_open'] = '<li class="prev page">';
+        //$config['prev_tag_close'] = '</li>';
+
+        //$config['cur_tag_open'] = '<span href="">';
+        //$config['cur_tag_close'] = '</span>';
+
+        //$config['num_tag_open'] = '<li class="page">';
+        //$config['num_tag_close'] = '</li>';
+
+        $config['display_pages'] = FALSE;
 
         $this->pagination->initialize($config);
         $page = ($this->uri->segment(2)) ? $this->uri->segment(2) : 0;
--- application/views/paginations/index.php
+++ application/views/paginations/index.php
@@ -1,15 +1,48 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
+
 <head>
-    <title>CodeIgniter Pagination</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+    <meta charset="utf-8">
+    <title>Pagination</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="#">
 </head>
+
 <body>
+    <nav class="navbar container-fluid sticky-top navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+            <img alt="logo jarditou" title="logo jarditou" src="<?= base_url("/assets/images/img_site/") . "88.png" ?>" width="" height="100" alt="">
+        </a>
+        <a class="navbar-brand" href="#">Accueil</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuprincipal" aria-controls="menuprincipal" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="menuprincipal">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Ajout produits</a>
+                </li>
+                <li class=" nav-item active">
+                    <a class="nav-link" href="#"></a>
+                </li>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link disabled" href="#">Déconnexion</a>
+                </li>
+            </ul>
+
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm" type="search" placeholder="Rechercher" aria-label="Rechercher">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
+            </form>
+
+        </div>
+    </nav>
     <div class="container">
         <h3 class="title is-3">CodeIgniter Database Pagination</h3>
         <div class="column">
+            <p> <?= $links ?></P>
             <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
                 <thead>
                     <tr>
@@ -29,8 +62,13 @@
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
-            </table>           
-               <p> <?= $links ?></P>    
+            </table>          
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="#"></script>
 </body>
+
+</html>
