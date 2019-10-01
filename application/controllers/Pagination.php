3cf7c1802cfe1d7db5f041cc8267dc4026b9b5a5
80010-92-01 <80010-92-01@exchange.ad.afpanet>
2019-10-01 16:44:41 +0200

    pagination

--- application/controllers/Pagination.php
+++ application/controllers/Pagination.php
@@ -7,23 +7,20 @@ class Pagination extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-
     }
 
     public function index()
     {
-
         $config = array();
-        $config["base_url"] = site_url() . "/pagination";
+        $config["base_url"] = site_url()."/pagination";   
         $config["total_rows"] = $this->pagination_model->get_counter();
         $config["per_page"] = 7;
         $config["uri_segment"] = 2;
        
-        
+        $config['use_page_number'] = true;
 
         //$config['full_tag_open'] = '<ul class="pagination">';
         //$config['full_tag_close'] = '</ul>';
-
 
         $config['first_link'] = false;
         //$config['first_tag_open'] = '';
@@ -40,27 +37,21 @@ class Pagination extends CI_Controller
         //$config['next_tag_close'] = '';
 
         $config['prev_link'] = false;
-        //config['prev_tag_open'] = '<li class="prev page">';
-        //$config['prev_tag_close'] = '</li>';
-
+        //config['prev_tag_open'] = '';
+        //$config['prev_tag_close'] = '';
      
         $config['cur_tag_open'] = '<a href="" class="btn btn-outline-primary" role="bouton">';
         $config['cur_tag_close'] = '</a>';
 
         $config['num_links'] = 10;
 
-        //$config['num_tag_open'] = '<li class="page">';
-        //$config['num_tag_close'] = '</li>';
-
-       
-
-        $this->pagination->initialize($config);
+        //$config['num_tag_open'] = '';
+        //$config['num_tag_close'] = '';
+        
+        $this->pagination->initialize($config);      
+        $data["links"] = $this->pagination->create_links();      
         $page = ($this->uri->segment(2)) ? $this->uri->segment(2) : 0;
-        $data["links"] = $this->pagination->create_links();
         $data['pagination'] = $this->pagination_model->get_prod($config["per_page"], $page);
-
-        var_dump($data['links']);
-        var_dump( $this->pagination_model->get_counter());
         $this->load->view('paginations/index', $data);
     }
 }
