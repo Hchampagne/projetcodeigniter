4103cd3aa318af3de97c7398f9e87feadd5de9c6
hchampagne <hchampagne56@gmail.com>
2019-10-03 21:18:39 +0200

    refonte index

--- application/views/index_C.php
+++ application/views/index_C.php
@@ -1,30 +1,50 @@
-<div>
-    <img src="<?= base_url("assets/images/img_site/")."promotion.jpg" ?>" alt="promotion" title="promotion" width="100%">
-</div>
-<div class="container-fluid"> 
 	<div class="row">
-		<div class="col-lg-6 col-xl-8">   
-		    <header><h1>Accueil</h1></header>
-		    <section>
-			    <h3>L'entreprise</h3>
-			    <p>Notre entreprise familiale met tout son savoir-faire à votre disposition dans le domaine du jardin et du paysagisme.</p>
-			    <p> Créée il y a 70 ans, notre entreprise vend fleurs, arbustes, matériel à main et motorisés.</p>
-			    <p>Implantés à Amiens, nous intervenons dans tout le département de la Somme : Albert, Doullens, Péronne, Abbeville, Corbie</p>
-		    </section>
-		    <section>
-		        <article>
-			        <h3>Qualité</h3>
-			        <p>Nous mettons à votre disposition un service personnalisé, avec 1 seul interlocuteur durant tout votre projet.</p>
-			        <p>Vous serez séduit par notre expertise, nos compétences et notre sérieux.</p>
-			    </article>
-			    <article>
-			        <h3>Devis gratuit</h3>
-			        <p>Vous pouvez bien sûr contacter pour de plus amples informations ou pour une demande d’intervention. Vous souhaitez un devis ? Nous vous le réalisons gratuitement.</p>
-		        </article>
-	        </section>
-        </div>	        
-	   <div class="col-lg-6 col-xl-4">Colonne avec une largeur de 4 blocs<div>
-            <img src="images/promotion.jpg" alt="promotion" title="promotion" width="100%">
-        </div>
-    </div>
-</div>
+		<div class="col text-center">
+			<img class="float-left" src="<?= base_url("assets/images/img_site/") . "truxton.png" ?>" alt="truxton" title="truxton" width="15%">
+
+
+			<img class="float-right" src="<?= base_url("assets/images/img_site/") . "bonneville.png" ?>" alt="bonneville" title="bonneville" width="15%">
+
+			<img src="<?= base_url("assets/images/img_site/") . "rocket.png" ?>" alt="rocket" title="rocket" width="15%">
+
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row  bg-darck">
+			<div class="col-lg-6 col-xl-8">
+				<section>
+					<h2>TRIUMPH BONNEVILLE T-120</h2>
+				</section><br>
+				<section>
+					<h6>UNE ICÔNE INTEMPORELLE RÉSOLUMENT MODERNE </h6>
+					<p>
+
+						La Bonneville originelle de 1959 est incontestablement une icône du monde motocycliste.
+						Adulée dans le monde entier par toutes les générations et tous les styles de pilotes, elle incarne pour beaucoup l’essence même de la moto.
+						La T120 et la T120 Black s'inspirent aujourd'hui du style originel et de la beauté de cette machine mythique pour charmer une nouvelle génération en alliant des performances,
+						des technologies et des équipements modernes au style et au caractère britannique authentiques de la Bonneville. </p>
+
+				</section><br>
+				<section>
+					<article>
+						<h6>UN STYLE INCOMPARABLE </h6>
+						<p>Un style emblématique, un vrai pedigree, de la prestance et des performances. </p>
+						<p>En combinant la puissance moderne du coupleux moteur Bonneville de 1 200 cm3,
+							un style et des finitions authentiques, une fabrication fidèle aux lignes et aux codes esthétiques intemporels de la Bonneville d'origine,
+							la T120 s'impose comme une icône historique ancrée dans la modernité.</p>
+						<p>La T120 Black reprend le look classique de la Bonneville T120,
+							 mais lui apporte un style urbain plus agressif et ténébreux avec une selle marron unique et des détails noirs,
+							  dont des jantes, une poignée passager, un échappement et une finition de peinture sophistiquée Matt Graphite ou Jet Black.</p>
+					</article><br>
+					<article>
+						<h3>Devis gratuit</h3>
+						<p>Vous pouvez bien sûr contacter pour de plus amples informations ou pour une demande d’intervention. Vous souhaitez un devis ? Nous vous le réalisons gratuitement.</p>
+					</article>
+				</section>
+			</div>
+			<div class="col-lg-6 col-xl-4">
+				<img src="<?= base_url("assets/images/img_site/") . "T120-black.jpg" ?>" alt="promotion" title="promotion" width="100%">
+			</div>
+		</div>
+	</div>
+	</div>
--- application/views/index_H.php
+++ application/views/index_H.php
@@ -1,37 +1,41 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
 	<meta charset="utf-8">
 	<title>index</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?= base_url("assets/css/style.css") ?>">
 </head>
-<body>
-	<nav class="navbar container-fluid sticky-top navbar-expand-lg navbar-dark bg-dark">
-		<a class="navbar-brand" href="#">
-			<img alt="logo" title="logo" src="<?= base_url("/assets/images/img_site/") . "logo.png" ?>" width="" height="100" alt="">
-		</a>
-		<a class="navbar-brand" href="<?= site_url("/Produits/Accueil/") ?>">Accueil</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuprincipal" aria-controls="menuprincipal" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="menuprincipal">
-			<ul class="navbar-nav mr-auto">
-				<li class="nav-item active">
-					<a class="nav-link" href="<?= site_url("produits/liste_user/") ?>">Boutique</a>
-				</li>
-				<li class=" nav-item active">
-					<a class="nav-link" href="#"></a>
-				</li>
-				</li>
-				<li class="nav-item active">
-					<a class="nav-link disabled" href=" #">contact</a>
-				</li>
-			</ul>
 
-			<form class="form-inline my-2 my-lg-0">
-				<input class="form-control mr-sm" type="search" placeholder="Rechercher" aria-label="Rechercher">
-				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
-			</form>
-		</div>
-	</nav>
+<body>
+	<header class="sticky-top">
+		<nav class="navbar container-fluid sticky-top navbar-expand-lg navbar-dark bg-dark">
+			<a class="navbar-brand" href="#">
+				<img alt="logo" title="logo" src="<?= base_url("/assets/images/img_site/") . "logo.png" ?>" width="" height="100" alt="">
+			</a>
+			<a class="navbar-brand" href="<?= site_url("/Produits/Accueil/") ?>">Accueil</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuprincipal" aria-controls="menuprincipal" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="menuprincipal">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="<?= site_url("Produits/liste_user/") ?>">Boutique</a>
+					</li>
+					<li class=" nav-item active">
+						<a class="nav-link" href="#"></a>
+					</li>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link disabled" href=" #">contact</a>
+					</li>
+				</ul>
+
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm" type="search" placeholder="Rechercher" aria-label="Rechercher">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
+				</form>
+			</div>
+		</nav>
+	</header>
